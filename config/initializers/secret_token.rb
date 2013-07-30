f9493d33952985d056c1aad4386ec8385235190d
Joshua Lee <jsungholee@gmail.com>
2013-07-29 21:47:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koreantyper::Application.config.secret_token = 'fb3e6f109f3b4f4736011246503f0fda68daad8e9728b50b40b4e816c9833df59d2e6ce47f063a471fd4f98243bb3d4b5c611aacabf6dfb7c6d57557a70b4f0b'
